e9f51e83784912ab73866fc13d67c81c8b9bab82
student <student@EPBYMIN2695.minsk.epam.com>
2018-11-12 17:50:31 +0300

    task1.py

---
+++ task1.py
@@ -0,0 +1,6 @@
+b = int(input())
+if 0 <= b <= 20:
+    for i in range(b):
+        print(i ** 2)
+else:
+    print("End")
---
+++ task4.py
@@ -0,0 +1,6 @@
+b = int(input())
+if 0 <= b <= 99:
+    for i in range(b):
+        print('%5s %6s %7s %8s' % ((i), oct(i), hex(i), (bin)i))
+else:
+    print("End")
