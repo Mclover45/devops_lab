5a07d659e40bdf3d845aa0661b88189984d72f33
student <student@EPBYMIN2695.minsk.epam.com>
2018-11-15 16:29:04 +0300

    homework4

--- task1.py
+++ task1.py
@@ -2,10 +2,32 @@ from configparser import ConfigParser
 import datetime
 import psutil
 import time
+
 parser = ConfigParser()
 parser.read('config.txt')
 Interval = (parser.get('some', 'Interval'))
 Cheker = 1
+
+
+class PsutilStatus(object):
+
+    def disk_usage(self):
+        return str(psutil.disk_usage("/").used / 1024 / 1024)
+
+    def virtual_memory(self):
+        return str(psutil.virtual_memory().used / 1024 / 1024)
+
+    def cpu_load(self):
+        return str(psutil.cpu_percent(interval=1))
+
+    def net_io_counters_1(self):
+        return str(psutil.net_io_counters().bytes_sent)
+
+    def net_io_counters_2(self):
+        return str(psutil.net_io_counters().bytes_recv)
+
+
+State = PsutilStatus()
 
 
 def Writer():
@@ -16,23 +38,20 @@ def Writer():
     file.write(" Date: ")
     file.write(str(datetime.datetime.utcnow()))
     file.write(" disk_usage MB ")
-    file.write(str(DU.used / 1000000))
+    file.write(str(State.disk_usage()))
     file.write(" VM MB ")
-    file.write(str(VM.used / 1000000))
+    file.write(str(State.virtual_memory()))
     file.write(" CpuLoad % ")
-    file.write(str(CpuLoad))
+    file.write(str(State.cpu_load()))
     file.write(" IO inform ")
     file.write(" bytes_sent ")
-    file.write(str(IOinform.bytes_sent))
+    file.write(str(State.net_io_counters_1()))
     file.write(" bytes_recv ")
-    file.write(str(IOinform.bytes_recv))
+    file.write(str(State.net_io_counters_2()))
     file.write("\n")
     file.close()
     Cheker += 1
+
 while True:
-    DU = psutil.disk_usage("/")
-    VM = psutil.virtual_memory()
-    CpuLoad = psutil.cpu_percent(interval=1)
-    IOinform = psutil.net_io_counters()
     time.sleep(int(Interval))  # in seconds
     Writer()
