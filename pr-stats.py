d143ed72ee3fa07b872d493b5bfb40aaa6b23d7c
Roman <roma-6134@mail.ru>
2018-11-17 18:58:59 +0300

    Homework5

---
+++ pr-stats.py
@@ -0,0 +1,25 @@
+import getpass
+import requests
+import sys
+
+user = input("Input Github Login: ")
+password = getpass.getpass()
+
+url = 'https://api.github.com/repos/alenaPy/devops_lab/pulls/48'
+req = requests.get(url, auth=(user, password))
+req = req.json()
+ans = (sys.argv)
+print(ans)
+
+if str(ans) == "['pr-stats.py', '--d']":
+    print('Created at:', req['created_at'])
+
+elif str(ans) == "['pr-stats.py', '--u']":
+    print('Created by:', req['user']['login'])
+
+elif str(ans) == "['pr-stats.py', '--c']":
+    print('Closed at:', req['closed_at'])
+elif str(ans) == "['pr-stats.py', '--s']":
+    print('Status:', req['labels'][0]['name'])
+elif str(ans) == "['pr-stats.py', '--help']":
+    print("Who created --u , When created --a, When closed --c, Status --s")
