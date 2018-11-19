125995957ef248a3d79b9a9ed1008ca45e9f405a
student <student@EPBYMIN2695.minsk.epam.com>
2018-11-19 15:54:45 +0300

    homvework6

--- task_vers.py
+++ task_vers.py
@@ -1,18 +1,16 @@
 import json
-from pip._internal.operations.freeze import freeze
 import site
 import subprocess
 import sys
 import yaml
 data = {'1. version of python': sys.version,
-     '2. virtualenv': sys.exec_prefix,
-     '3. python executable location': sys.executable,
-     '4. pip location': str(subprocess.getoutput("which pip")),
-     '5. PYTHONPATH': sys.path,
-     '6. installed packages (name, version)': 
-     next(i for i in sys.path if "site-packages" in i),
-     '7. site-packages location': site.getsitepackages()
-     }
+        '2. virtualenv': sys.exec_prefix,
+        '3. python executable location': sys.executable,
+        '4. pip location': str(subprocess.getoutput("which pip")),
+        '5. PYTHONPATH': sys.path,
+        '6. installed packages (name, version)':
+            next(i for i in sys.path if "site-packages" in i),
+        '7. site-packages location': site.getsitepackages()}
 with open('data.yml', 'w') as outfile:
     yaml.dump(data, outfile, default_flow_style=False)
 with open('data.json', 'w') as outfile:
--- utest_polindrom.py
+++ utest_polindrom.py
@@ -1,19 +1,13 @@
-import unittest
 import polindrom
-import coverage
+import unittest
+
 
 class test_polindrom(unittest.TestCase):
-    def setUp(self):
-
-        """Init"""
 
     def test_polindrom(self):
-
         self.assertFalse(polindrom.polin("zooooooosdfsdfooz"))
         self.assertTrue(polindrom.polin("zoooooooooz"))
 
-    def tearDown(self):
 
-        """Finish"""
 if __name__ == '__main__':
     unittest.main()
