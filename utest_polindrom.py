43393a96a43781b4ad956729db2b62ff925996bb
Mclover45 <44427932+Mclover45@users.noreply.github.com>
2018-11-19 16:53:47 +0300

    Update utest_polindrom.py

--- utest_polindrom.py
+++ utest_polindrom.py
@@ -7,7 +7,3 @@ class test_polindrom(unittest.TestCase):
     def test_polindrom(self):
         self.assertFalse(polindrom.polin("zooooooosdfsdfooz"))
         self.assertTrue(polindrom.polin("zoooooooooz"))
-
-
-if __name__ == '__main__':
-    unittest.main()
